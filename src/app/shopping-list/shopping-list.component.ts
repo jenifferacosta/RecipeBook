fe5d0df7bb3d539ee9f72113ccf409b99eaa44cf
Jeniffer Acosta <jeniffer.acosta@icloud.com>
2020-09-22 18:14:19 +1000

    dropDown directive added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -19,7 +19,7 @@ export class ShoppingListComponent implements OnInit {
   }
 
   onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+    this.ingredients.push(ingredient );
   }
 
 }
